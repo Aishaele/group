31c3b2fef5f1e7557653dde4e9674d3939ae0a03
iamaisha <nanaaishaeleamodu@gmail.com>
2022-11-20 20:06:09 -0800

    made correction

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -5,9 +5,9 @@ import styles from "./Style/Hero.css"
 const Hero = () => {
   return (
    <body>
-    <div className='hero'>
-    <div className='hero_writeup'>
-        <h3 className='feature_line'> FEATURES</h3>
+    <div className='hero_feature'>
+    <div className='hero_writeup_feature'>
+        <h3 className='feature_line_feature'> FEATURES</h3>
         <h1> Explore features <br/> of Weatherly </h1>
         <p>Stay informed about weather events and 
         <br /> protected with Weathery forecasts in real-time.</p>
